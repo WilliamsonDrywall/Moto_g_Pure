5f11e9976a79909d6660bef52c36712d7a72f950
Bamn213 <94124465+Bamn213@users.noreply.github.com>
2022-08-26 08:59:47 -0400

    Create omni_ellis.mk

---
+++ omni_ellis.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace ${_CODENAME_} with your Device Name's Value.
+# Replace ${_VENDORNAME_} with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/motorola/ellis/prebuilt/kernel:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := ellis
+PRODUCT_NAME := omni_ellis_t
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := ellis_t
+PRODUCT_MANUFACTURER := motorola
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT==motorola/ellis_t/ellis:11/RRHS31.Q3-46-110-10/d67faa:user/release-keys
+    PRIVATE_BUILD_DESC=ellis_t-user 11 RRHS31.Q3-46-110-10 d67faa release-keys
