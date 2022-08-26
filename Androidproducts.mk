f4c6ca3b08580c447773194d7e1d9b9792085b4a
Bamn213 <94124465+Bamn213@users.noreply.github.com>
2022-08-26 06:03:46 -0400

    Create Androidproducts.mk

---
+++ Androidproducts.mk
@@ -0,0 +1,5 @@
+# Replace ${_CODENAME_} with your Device Name's Value.
+# Replace ${_VENDORNAME_} with your Brand/Vendor/Manufacturer's Value.
+# The part of last line in mine looks like "omni_Primo_RX5.mk"
+
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_ellis_t.mk
